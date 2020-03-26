31fc92dcc8825f03d5a3ad3259357e1cb8b42ad1
Avijit95 <avijit.influxiq@gmail.com>
2020-03-26 15:52:07 +0530

    Avijit

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -8,8 +8,8 @@ import {environment} from '../../../environments/environment';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  public logo: any = '';
-  public fromTitle: any = 'Login Here';    // This is a From Title
+  public logo: any = '../../assets/images/logo.png';
+  public fromTitle: any = 'InfluxIQ PM Tool';    // This is a From Title
   public fullUrl: any = environment.apiBaseUrl;  // server url
   public endpoint: any = 'login';
   public buttonName: any = 'Login Button';
